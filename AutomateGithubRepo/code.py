98f8591ee2b9444e535134bb3bdf78e99729c159
Uday-sidagana <uday.sidgana@gmail.com>
2024-09-15 00:04:35 +0530

    Function to create a Folder using os module

---
+++ AutomateGithubRepo/code.py
@@ -0,0 +1,8 @@
+import os
+
+def folder():
+   
+    directory= input("Directory Format: /Users/macbookair/Desktop/python/AutomationPython/AutomateGithubRepo/ ")
+    os.mkdir(directory)
+    
+folder()
