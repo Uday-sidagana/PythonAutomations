c1f86a99ac085feb569f5057e328993ba2240661
Uday-sidagana <uday.sidgana@gmail.com>
2024-09-15 00:17:39 +0530

    Added .env file and imported .env modules and requests for Github API Connection

--- AutomateGithubRepo/code.py
+++ AutomateGithubRepo/code.py
@@ -1,8 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+import requests
+
 def folder():
    
     directory= input("Directory Format: /Users/macbookair/Desktop/python/AutomationPython/AutomateGithubRepo/ ")
     os.mkdir(directory)
     
-folder()
